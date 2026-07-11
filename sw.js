989f70fbf3b308eba3499db59dcf02882461223f
Skylim777 <cobhc.kissing.the.shadows@gmail.com>
2026-07-11 14:20:40 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // ChordSketch - sw.js オフライン用キャッシュ（PWA）
 // コードを更新して公開し直すときは CACHE の番号を v2, v3… と上げる
-const CACHE = "chordsketch-v8";  // 鼻歌解析を作り直してv8に
+const CACHE = "chordsketch-v9";  // 音程検出をYIN方式に変更してv9に
 const FILES = [
   "./",
   "./index.html",
